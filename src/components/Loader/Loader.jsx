f34ab57e96442a2377e57adcaae899458b11820b
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-09-20 21:50:36 +0200

    install and style react-loader-spinner

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,18 @@
+import { Grid } from 'react-loader-spinner';
+
 const Loader = () => {
-  return <div>Loader Component</div>;
+  return (
+    <Grid
+      height="80"
+      width="80"
+      color="#6e78e8"
+      ariaLabel="grid-loading"
+      radius="12.5"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />
+  );
 };
 
 export default Loader;
