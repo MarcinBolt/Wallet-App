d478713b044db314fc4d45ad0f75984c27fb4812
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-09-20 16:50:23 +0200

    add user.service, transaction.service

---
+++ server/service/transactions.service.js
@@ -0,0 +1,17 @@
+import Transaction from './schema/transaction.schema.js';
+
+export const getTransactions = async owner => await Transaction.find({ owner });
+
+export const createTransaction = async (date, year, month, type, category, comment, sum, owner) =>
+  await Transaction.create({ date, year, month, type, category, comment, sum, owner });
+
+export const updateTransactionById = async (id, field) =>
+  await Transaction.findByIdAndUpdate({ _id: id }, field, { new: true });
+
+export const deleteTransactionById = async id => await Transaction.deleteOne({ _id: id });
+
+export const getTransactionsByKey = async (owner, category) =>
+  await await Transaction.find({ owner, category });
+
+export const getStatistics = async (owner, year, month) =>
+  await Transaction.find({ owner, year, month });
