86b48f8b249e293b9d12b1f14b98819a98fe867e
Joanna K <joannakleinrm@gmail.com>
2023-09-20 23:07:30 +0200

    Chart- chart base and labels created

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,5 +1,76 @@
+import React from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    Tooltip,
+    Legend,
+    ArcElement,
+} from 'chart.js';
+
+
+ChartJS.register(
+    Tooltip,
+    Legend,
+    ArcElement
+);
+
+const data = {
+    labels: ["Main expanses", "Products", "Car", "Self care", "Child care", "Household products", "Education", "Leisure", "Other expenses"],
+    datasets: [
+    {
+      label: 'Expanses',
+      data: [120, 19, 35, 5, 15, 33,45, 20, 11],
+      backgroundColor: [
+        '#FED057',
+        '#FFD8D0',
+        '#FD9498',
+        '#C5BAFF',
+        '#6E78E8',
+        '#4A56E2',
+        '#81E1FF',
+        '#24CCA7',
+        '#00AD84'
+      ],
+      borderColor: [
+        '#FED057',
+        '#FFD8D0',
+        '#FD9498',
+        '#C5BAFF',
+        '#6E78E8',
+        '#4A56E2',
+        '#81E1FF',
+        '#24CCA7',
+        '#00AD84'
+      ],
+        borderWidth: 1,
+        weight: 300,
+    }],  
+}
+
+const options = {
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      position: 'right',
+      labels: {
+        boxWidth: 12,
+      }
+    },
+  }
+};
+
 const Chart = () => {
-  return <div>Chart Component</div>;
+    return(
+      <div style={{ width: '50vw', height: '50vh' }}>
+        <Labels />
+        <Doughnut
+            data={data}
+            width={300}
+            height={300}
+            options={options}
+        />
+        </div>
+    )
 };
 
 export default Chart;
---
+++ src/components/Chart/Labels.jsx
@@ -0,0 +1,32 @@
+export const Labels = () => {
+    return (
+        <div>
+            <label htmlFor="month"></label>
+                <select name="month" id="month" >
+                <option value=""></option>
+                <option value="January">January</option>
+                <option value="February">February</option>
+                <option value="March">March</option>
+                <option value="April">April</option>
+                <option value="May">May</option>
+                <option value="June">June</option>
+                <option value="July">July</option>
+                <option value="August">August</option>
+                <option value="September">September</option>
+                <option value="October">October</option>
+                <option value="November">November</option>
+                <option value="December">December</option>
+            </select>
+    
+            <label htmlFor="year"></label>
+                <select name="year" id="year">
+                <option value=""></option>
+                <option value="2023">2023</option>
+                <option value="2022">2022</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+                <option value="2019">2019</option>
+            </select>
+        </div>
+    )
+}
