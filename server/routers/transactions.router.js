904baacae902431b048bf7a087e7980ea5d7d8ed
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-09-21 18:40:05 +0200

    Update transactions.router.js

--- server/routers/transactions.router.js
+++ server/routers/transactions.router.js
@@ -2,17 +2,17 @@ import { Router } from 'express';
 
 const transactionRouter = Router();
 
-transactionRouter.get('/' /*to do auth, getOwnersTransactions*/);
+transactionRouter.get('/' /*to do auth, getOwnerTransactions*/);
 
-transactionRouter.post('/' /*to do auth, createOwnersTransaction*/);
+transactionRouter.post('/' /*to do auth, createOwnerTransaction*/);
 
-transactionRouter.get('/:id' /*to do auth, getOwnersTransactionById*/);
+transactionRouter.get('/:id' /*to do auth, getOwnerTransactionById*/);
 
-transactionRouter.put('/:id' /*to do auth, updateOwnersTransactionById*/);
+transactionRouter.put('/:id' /*to do auth, updateOwnerTransactionById*/);
 
-transactionRouter.delete('/:id' /*to do auth, deleteOwnersTransactionById*/);
+transactionRouter.delete('/:id' /*to do auth, deleteOwnerTransactionById*/);
 
-transactionRouter.get('/:category' /*to do auth, getOwnersTransactionsByCategory*/);
+transactionRouter.get('/:category' /*to do auth, getOwnerTransactionsByCategory*/);
 
 //proponuję dać rok i miesiąc w params, a nie w query string
 transactionRouter.get('/statistics/:year/:month' /*to do auth, getOwnersTransactionsByDate*/);
