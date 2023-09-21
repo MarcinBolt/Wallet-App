c61181022ef742ca8b53b6b7189a5f9f70861525
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-09-21 18:19:59 +0200

    Update transactions.router.js

--- server/routers/transactions.router.js
+++ server/routers/transactions.router.js
@@ -2,19 +2,19 @@ import { Router } from 'express';
 
 const transactionRouter = Router();
 
-transactionRouter.get('/' /*to do auth, getTransactions*/);
+transactionRouter.get('/' /*to do auth, getOwnersTransactions*/);
 
-transactionRouter.post('/' /*to do auth, createTransaction*/);
+transactionRouter.post('/' /*to do auth, createOwnersTransaction*/);
 
-transactionRouter.get('/:id' /*to do auth, getTransactionById*/);
+transactionRouter.get('/:id' /*to do auth, getOwnersTransactionById*/);
 
-transactionRouter.put('/:id' /*to do auth, updateTransactionById*/);
+transactionRouter.put('/:id' /*to do auth, updateOwnersTransactionById*/);
 
-transactionRouter.delete('/:id' /*to do auth, deleteTransactionById*/);
+transactionRouter.delete('/:id' /*to do auth, deleteOwnersTransactionById*/);
 
-transactionRouter.get('/:category' /*to do auth, getTransactionsByCategory*/);
+transactionRouter.get('/:category' /*to do auth, getOwnersTransactionsByCategory*/);
 
 //proponuję dać rok i miesiąc w params, a nie w query string
-transactionRouter.get('/statistics/:year/:month' /*to do auth, getTransactionsByDate*/);
+transactionRouter.get('/statistics/:year/:month' /*to do auth, getOwnersTransactionsByDate*/);
 
 export default transactionRouter;
