a56488c65d3287e89cd9c4457291a551a10dca2a
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-09-21 11:14:46 +0200

    add container from mui

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,28 @@
+import Container from '@mui/material/Container';
 import { Grid } from 'react-loader-spinner';
 
 const Loader = () => {
   return (
-    <Grid
-      height="80"
-      width="80"
-      color="#6e78e8"
-      ariaLabel="grid-loading"
-      radius="12.5"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-    />
+    <Container
+      maxWidth="sx"
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+      }}
+    >
+      <Grid
+        height="80"
+        width="80"
+        color="#6e78e8"
+        ariaLabel="grid-loading"
+        radius="12.5"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </Container>
   );
 };
 
