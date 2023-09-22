9689edbf71b5b9e89975e39e8927afc638e0ca0c
Damian Dawidowski <damiandawidowski@gmail.com>
2023-09-22 14:14:23 +0200

    Cleanup of empty import  	modified:   src/components/LoginForm/LoginForm.jsx  	modified:   src/pages/LoginPage/LoginPage.module.css

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,31 +1,21 @@
 //TODO
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useDispatch } from 'react-redux';
 import { logIn } from '../../redux/auth/operations';
 import logo from '../../assets/images/apple-touch-icon.png';
 import css from "./LoginForm.module.css";
-import btn from "./MainButton.module.css";
- 
-import * as Yup from "yup";
-import { object, string } from 'yup';
+import btn from "./MainButton.module.css"; 
+import * as Yup from "yup"; 
 import { Formik, Form, Field } from 'formik';
 import { NavLink } from 'react-router-dom';
 import MainButton from './MainButton';
-
-const theme = createTheme();
-
+ 
 const LoginForm = () => {
   const dispatch = useDispatch();
 
   const validationsSchema = Yup.object().shape({
     email: Yup.string("Please enter an e-mail")
-    .email("Please enter a valid e-mail")
-    .required("Email is required!"),
+      .email("Please enter a valid e-mail")
+      .required("Email is required!"),
     password: Yup.string("Please enter a password")
       .min(6, "The password must be at least 6 characters long")
       .max(12, "The password must not be longer then 12 characters")
