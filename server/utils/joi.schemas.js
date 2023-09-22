9b9eef69e20e97253159fa3290a2d52446248d4d
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-09-22 18:33:45 +0200

    Update joi.schemas.js
    add schemas for user

--- server/utils/joi.schemas.js
+++ server/utils/joi.schemas.js
@@ -1,5 +1,20 @@
 import Joi from 'joi';
 
+export const userRegisterReqBodySchema = Joi.object({
+  email: Joi.string().email({ minDomainSegments: 2 }).required(),
+  password: Joi.string().min(7).required(),
+  firstName: Joi.string().min(3).max(30).required(),
+});
+
+export const userLoginReqBodySchema = Joi.object({
+  email: Joi.string().email({ minDomainSegments: 2 }).required(),
+  password: Joi.string().min(7).required(),
+});
+
+export const userEmailReqBodySchema = Joi.object({
+  email: Joi.string().email({ minDomainSegments: 2 }).required(),
+});
+
 export const transactionBodySchema = Joi.object({
   date: Joi.string().required(),
   year: Joi.string().required(),
