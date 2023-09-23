89624700e4d05a896d4bbb9a6abc6ac04eb986fb
JonaszChudobski <jonasz.chudobski@wp.pl>
2023-09-23 16:55:58 +0200

    route for dollar button changed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,9 @@
-import CurrencyTable from '../CurrencyTable/CurrencyTable';
 import Chart from '../Chart/Chart';
 import Transactions from '../Transactions/Transactions';
 
 const Home = () => {
   return (
     <>
-      <CurrencyTable />
       <Chart />
       <Transactions />
     </>
