fbfea8d84f9e2b527e39845ab3cc8e182ad9d95b
Julia-Kaim <julia.kaim08@gmail.com>
2023-09-23 21:59:39 +0200

    feature Balance

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,18 +1,46 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 export default function Balance() {
   return (
-    <Box
+    <Container
       sx={{
-        width: 336,
-        height: 80,
+        maxWidth: 395,
         backgroundColor: 'white',
+     
         borderRadius: 30,
-        marginBottom: 10,
+        height: 'auto',
+        textTransform: 'up  percase',
+        marginBottom: 32,
+        fontSize: 12,
       }}
     >
-      Your Balance ₴
-    </Box>
+      <Box
+        sx={{
+          fontSize: 12,
+          fontWeight: 400,
+
+          color: '#a6a6a6',
+
+          fontWeight: 'bold',
+        }}
+      >
+        Your Balance
+        <Box
+          sx={{
+            fontSize: 30,
+
+            fontWeight: 400,
+
+            color: 'black',
+
+            fontWeight: 'bold',
+          }}
+        >
+          ₴
+        </Box>
+      </Box>
+    </Container>
   );
 }
--- src/components/CurrencyTable/CurrencyTable.jsx
+++ src/components/CurrencyTable/CurrencyTable.jsx
@@ -1,47 +1,9 @@
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-
-export default function CurrencyTable() {
+const CurrencyTable = () => {
   return (
     <div>
-      <TableContainer
-        sx={{
-          backgroundColor: '#4A56E2',
-          borderRadius: '30px',
-          color: 'white',
-          width: 336,
-          height: 182,
-        }}
-      >
-        <Table
-         
-        >
-          <TableHead>
-            <TableRow
-              sx={{
-                backgroundColor: 'white',
-                opacity: 0.2,
-                borderRadiusTop: '30px',
-                display: 'flex',
-                justifyContent: 'space-between',
-              }}
-            >
-              <TableCell>Currency</TableCell>
-              <TableCell>Purchase</TableCell>
-              <TableCell>Sales</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell>.</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <h2>CurrencyTable Component</h2>
     </div>
   );
-}
+};
+
+export default CurrencyTable;
