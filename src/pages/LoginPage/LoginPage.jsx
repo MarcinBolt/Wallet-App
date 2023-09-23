aaaaf1bcc78f97ac7ac9eea2cbd5c908bc904f51
Damian Dawidowski <damiandawidowski@gmail.com>
2023-09-23 17:42:44 +0200

    modified:   src/App.css  	modified:   src/pages/LoginPage/LoginPage.jsx  	modified:   src/pages/LoginPage/LoginPage.module.css

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -5,11 +5,13 @@ import css from "./LoginPage.module.css";
   //      import LoginForm from "../components/LoginForm/LoginForm_altt";
 const LoginPage = () => {
     return (
-    <div> 
+    <div>  
       <div className={css.page_wrapper}> 
+      <div className={css.subBackround}></div>
         <div className={css.form_wrapper}> 
           <p className={css.page_text}>Finance App</p>
           <div className={css.frame}></div>
+          
           <LoginForm />
         </div> 
       </div> 
