320ca4fdfa5b671ad1a2c2b99949248d48946e42
Joanna K <joannakleinrm@gmail.com>
2023-09-24 17:56:10 +0200

    StatsLables created

---
+++ src/components/Chart/StatsLables/StatsLabels.jsx
@@ -0,0 +1,34 @@
+import css from './Chart.module.css';
+
+export const Labels = () => {
+    return (
+        <div className={css.lablesContainer} style={{}}>
+            <label htmlFor="month" ></label>
+            <select name="month" id="month" className={css.label}>
+                <option value=""></option>
+                <option value="January">January</option>
+                <option value="February">February</option>
+                <option value="March">March</option>
+                <option value="April">April</option>
+                <option value="May">May</option>
+                <option value="June">June</option>
+                <option value="July">July</option>
+                <option value="August">August</option>
+                <option value="September">September</option>
+                <option value="October">October</option>
+                <option value="November">November</option>
+                <option value="December">December</option>
+            </select>
+    
+            <label htmlFor="year" ></label>
+            <select name="year" id="year" className={css.label}>
+                <option value=""></option>
+                <option value="2023">2023</option>
+                <option value="2022">2022</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+                <option value="2019">2019</option>
+            </select>
+        </div>
+    )
+}
