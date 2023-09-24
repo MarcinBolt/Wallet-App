fc4b767c387e2439c3311e48aecdda6f1d48043a
Joanna K <joannakleinrm@gmail.com>
2023-09-24 17:41:30 +0200

    statsHeader title added

---
+++ src/components/Chart/StatsHeader/StatsHeader.jsx
@@ -0,0 +1,11 @@
+import css from './StatsHeader.module.css'
+
+const StatsHeader = () => {
+  return (
+    <div className={css.statsHeader}>
+      <h2>Statistics</h2>
+    </div>
+  );
+};
+
+export default StatsHeader;
