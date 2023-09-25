f94ce521f389d9f332eb4e24dbd2c48ab2bd9d79
Julia-Kaim <julia.kaim08@gmail.com>
2023-09-25 21:25:12 +0200

    fix: app

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Logo from './components/Logo/Logo.jsx';
 const Login = lazy(() => import('./pages/Login.jsx'));
 const Register = lazy(() => import('./pages/Register.jsx'));
 const Dashboard = lazy(() => import('./components/Dashboard/Dashboard.jsx'));
-const BtnDelete = lazy(() => import('./components/ButtonDelete/ButtonDelete.jsx'));
+
 const NotFound = lazy(() => import('./pages/NotFound.jsx'));
 
 const App = () => {
@@ -37,7 +37,7 @@ const App = () => {
           element={<PrivateRoute component={<Dashboard />} redirect={'/'} />}
         />
         //TODO
-        <Route exact path={`/`} element={<PublicRoute component={<BtnDelete />} />} />
+        
         <Route exact path={`/`} element={<PublicRoute component={<Login />} />} />
         <Route exact path={`/login`} element={<PublicRoute component={<Login />} />} />
         <Route exact path={`/register`} element={<PublicRoute component={<Register />} />} />
