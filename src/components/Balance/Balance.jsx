ea5b82591d6ba5319e2e5aa75c459ff56ad24bf8
Julia-Kaim <julia.kaim08@gmail.com>
2023-09-25 21:42:55 +0200

    fix: module.css

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -3,49 +3,13 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
+import css from './Balance.module.css';
+
 export default function Balance() {
   return (
-    <Container
-      sx={{
-        '@media (min-width: 780px)': {
-          width: '360px',
-          padding: '8px 40px 11px',
-        },
-        '@media (min-width: 1200px)': {
-          width: '395px',
-        },
-        width: '280px',
-        backgroundColor: 'white',
-        borderRadius: 30,
-        height: 80,
-        textTransform: 'uppercase',
-        padding: '8px 32px 11px',
-      }}
-    >
-      <Box
-        sx={{
-          fontSize: 12,
-          fontWeight: 400,
-          color: '#a6a6a6',
-
-          display: 'flex',
-          alignItems: 'start',
-        }}
-      >
-        Your Balance
-      </Box>
-      <Box
-        sx={{
-          fontSize: 30,
-          fontWeight: 700,
-          color: 'black',
-
-          display: 'flex',
-          alignItems: 'start',
-        }}
-      >
-        ₴
-      </Box>
+    <Container className={css.BalanceContainer}>
+      <Box className={css.BalanceText}>Your Balance</Box>
+      <Box className={css.BalanceBox}>₴</Box>
     </Container>
   );
 }
