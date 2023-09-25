3ee16c04b9660078b335179d164672a15c3e37dc
Joanna K <joannakleinrm@gmail.com>
2023-09-26 00:38:00 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,13 @@
 import Chart from "./Chart/Chart";
+import StatsSelectList from "./StatsSelectList/StatsSelectList";
+import StatsSummary from "./StatsSummary/StatsSummary";
+import StatsTable from "./StatsTable/StatsTable";
 
 const Statistics = () => {
   return (
     <div className={css.statisticsContainer}>
       <div>
-        <StatsHeader/>
+        {/* <StatsHeader/> to zaimportowania z innego brancha */}
         <Chart />
       </div>
       <div>
