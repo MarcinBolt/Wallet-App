682233d1d7006f48c62f652eed0bb749d96e732d
Joanna K <joannakleinrm@gmail.com>
2023-09-26 00:08:25 +0200

    stats title styles update plus dirchange

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import AppBar from '../AppBar/AppBar';
 import CurrencyTable from '../CurrencyTable/CurrencyTable.jsx';
 import Chart from '../Chart/Chart.jsx';
 import Transactions from '../Transactions/Transactions.jsx';
-import StatsHeader from '../StatsHeader/StatsHeader';
+import StatsHeader from '../Statistics/StatsHeader/StatsHeader';
 
 const Dashboard = () => {
   return (
--- src/components/Statistics/StatsHeader/StatsHeader.jsx
+++ src/components/Statistics/StatsHeader/StatsHeader.jsx
@@ -3,7 +3,7 @@ import css from './StatsHeader.module.css'
 const StatsHeader = () => {
   return (
     <div >
-      <h2 className={css.statsHeader}>Statistics</h2>
+      <h2 className={css.statisticsHeader}>Statistics</h2>
     </div>
   );
 };
