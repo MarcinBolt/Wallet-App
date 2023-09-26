72cabaf2a3618b8c0db4611154ebb238298bd539
Julia-Kaim <julia.kaim08@gmail.com>
2023-09-26 20:09:22 +0200

    fix: css

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,6 @@
 import Button from '@mui/material/Button';
 
-export default function ModalButton({
-  type,
-  content,
-  onClick = null,
-  disabled = false,
-  hasaccent = false,
-}) {
+const MainButton = ({ type, content, onClick = null, disabled = false, hasaccent = false }) => {
   const setBgColor = hasaccent => (hasaccent ? `#24CCA7` : `#ffffff`);
   const setColor = hasaccent => (hasaccent ? `#ffffff` : `#4A56E2`);
   const setBorder = hasaccent => (hasaccent ? `#24CCA7` : `#4A56E2`);
@@ -17,25 +11,16 @@ export default function ModalButton({
       onClick={onClick}
       disabled={disabled}
       hasAccent={hasaccent}
+      className={css.MainButton}
       sx={{
-        textTransform: 'uppercase',
         backgroundColor: setBgColor(hasaccent),
         color: setColor(hasaccent),
         border: `1px solid ${setBorder(hasaccent)}`,
-        width: '300px',
-        height: '50px',
-        borderRadius: '20px',
-
-        cursor: 'pointer',
-        '&:hover:not(:disabled)': {
-          boxShadow: '8px 8px 24px 0px rgba(66, 68, 90, 1)',
-        },
-        '&:disabled': {
-          opacity: 0.35,
-        },
       }}
     >
       {content}
     </Button>
   );
-}
+};
+
+export default MainButton;
