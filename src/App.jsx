3738739ea5530e6a835ffe7660c410166fc94a2a
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-09-28 00:20:22 +0200

    routing fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ import PrivateRoute from './components/PrivateRoute';
 import Loader from './components/Loader/Loader.jsx';
 import './App.css';
 import SharedLayout from './components/SharedLayout/SharedLayout.jsx';
-// import CurrencyTable from './components/CurrencyTable/CurrencyTable.jsx';
-// import DiagramTab from './components/DiagramTab/DiagramTab.jsx';
-// import HomeTab from './components/HomeTab/HomeTab.jsx';
+import HomeTab from './components/HomeTab/HomeTab.jsx';
+import CurrencyTable from './components/CurrencyTable/CurrencyTable.jsx';
+import DiagramTab from './components/DiagramTab/DiagramTab.jsx';
 
 const Login = lazy(() => import('./pages/LoginPage/LoginPage.jsx'));
 const Register = lazy(() => import('./pages/RegisterPage/RegisterPage.jsx'));
 const Dashboard = lazy(() => import('./pages/Dashboard/Dashboard.jsx'));
 const NotFound = lazy(() => import('./pages/NotFound.jsx'));
-const VerifyEmail = lazy(() => import('./pages/VerifyEmail.jsx'))
+const VerifyEmail = lazy(() => import('./pages/VerifyEmail.jsx'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -33,13 +33,25 @@ const App = () => {
     <Suspense fallback={<Loader />}>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
-          <Route exact index element={<PublicRoute redirectTo={<Navigate to="/dashboard" replace />} component={<Login />} />} />
-          <Route path="/register" element={<PublicRoute component={<Register />} redirectTo={<Navigate to="/dashboard" replace />} />} />
-          <Route path="/login" element={<PublicRoute redirectTo={<Navigate to="/dashboard" replace />} component={<Login />} />} />
-          <Route path="/dashboard" element={<PrivateRoute redirectTo={<Navigate to="/login" replace />} component={<Dashboard />} />} />
-          <Route path={`/users/verify/:verificationToken`} element={<PublicRoute component={<VerifyEmail />} />} />
+          <Route exact path={`/dashboard`} element={<PrivateRoute component={<Dashboard />} />}>
+            <Route exact index element={<PrivateRoute component={<HomeTab />} />} />
+            <Route
+              exact
+              path={'statistics'}
+              element={<PrivateRoute component={<DiagramTab />} />}
+            />
+            <Route
+              exact
+              path={'currency'}
+              element={<PrivateRoute component={<CurrencyTable />} />}
+            />
           </Route>
-        <Route path="*" element={<NotFound />} />
+          <Route exact index element={<PublicRoute component={<Login />} />} />
+          <Route exact path={`/login`} element={<PublicRoute component={<Login />} />} />
+          <Route exact path={`/register`} element={<PublicRoute component={<Register />} />} />
+          <Route path={`/verify`} element={<PublicRoute component={<VerifyEmail />} />} />
+          <Route path="*" element={<PublicRoute component={<NotFound />} />} />
+        </Route>
       </Routes>
     </Suspense>
   );
