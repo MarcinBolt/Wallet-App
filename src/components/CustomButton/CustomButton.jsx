67812389d5e9dcb95973202f368f3f0f3de7e64d
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-09-28 11:29:46 +0200

    update path to styling

--- src/components/CustomButton/CustomButton.jsx
+++ src/components/CustomButton/CustomButton.jsx
@@ -1,4 +1,4 @@
-import css from './MainButton.module.css';
+import css from './CustomButton.module.css';
 // color === 'primary' | 'secondary'
 const CustomButton = ({ type, content, onClick = null, color }) => {
   const btnClass = color => `${color === 'primary' ? css.buttonPrimary : css.buttonSecondary}`;
