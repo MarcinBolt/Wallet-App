df25196d0daf9c248967740693857a7d7222d5b5
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-09-28 14:17:19 +0200

    CustomButton finall upgrade

--- src/components/CustomButton/CustomButton.jsx
+++ src/components/CustomButton/CustomButton.jsx
@@ -1,7 +1,12 @@
 import css from './CustomButton.module.css';
-// color === 'primary' | 'secondary'
-const CustomButton = ({ type, content, onClick = null, color }) => {
-  const btnClass = color => `${color === 'primary' ? css.buttonPrimary : css.buttonSecondary}`;
+
+// How to use CustomButton?
+
+// The component takes the following props: type (e.g., button, submit), color (primary or secondary), content (button label), onClick (callback function).
+
+const CustomButton = ({ type, color, content, onClick = null }) => {
+  const btnClass = color =>
+    `${ color === 'primary' ? css.buttonPrimary : color === 'secondary' ? css.buttonSecondary : null }`;
   return (
     <button type={type} onClick={onClick} className={btnClass(color)}>
       {content}
