c496a329c8c430ad8b0e43f5ddd14f7f50f20708
MarcinBolt <marcin.boltruczyk@gmail.com>
2023-09-29 13:15:50 +0200

    add z-index = 5 to the button module.css

--- src/components/ButtonAddTransactions/ButtonAddTransaction.jsx
+++ src/components/ButtonAddTransactions/ButtonAddTransaction.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import css from './ButtonAddTransaction.module.css';
 export const ButtonAddTransaction = ({ handleClick }) => {
   return (
