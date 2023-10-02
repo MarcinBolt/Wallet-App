f7f8ffaaeb0fc09503d37c02aeddce8c567fa47b
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-10-02 22:36:16 +0200

    Styling balance, add logic

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,52 +1,35 @@
 import Box from '@mui/material/Box';
-import css from './Balance.module.css';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectBalance, selectTransactions } from '../../redux/selectors';
 import { updateBalance } from '../../redux/transactions/transactions.slice';
 import { formatMoney } from '../../utils/formatMoney';
 import { fetchTransactions } from '../../redux/transactions/transactions.operations';
+import css from './Balance.module.css';
 
 const Balance = () => {
-   const transactions = useSelector(selectTransactions);
-  const [balance, setBalance] = useState();
+  const transactions = useSelector(selectTransactions);
+  const [balance, setBalance] = useState(0);
 
   useEffect(() => {
-    countBalance(transactions);
-  }, []);
-
-  useEffect(() => {
-    countBalance(transactions);
-  }, [transactions]);
-
-  const countBalance = transactions => {
-    console.log(transactions)
-    const incomesSum = [...transactions].reduce((acc, transaction) => {
-      if (transaction.type === 'Income') {
-        acc + transaction.sum;
-      }
-      return acc;
-    }, 0);
-    console.log('incomes from func:', incomesSum);
-    const expensesSum = [, , , transactions].reduce((acc, transaction) => {
-      if (transaction.type === 'Expense') {
-        acc + transaction.sum;
-      }
-      return acc;
-    }, 0);
-    console.log('expenses from func:', expensesSum);
-    const newBalance = incomesSum - expensesSum;
-    console.log('typeof balance from func:', typeof newBalance);
-    console.log('balance from func:', newBalance);
+    const incomesSum = [...transactions]
+      .filter(t => t.type === 'Income')
+      .reduce((acc, t) => {
+        return acc + t.sum * 100;
+      }, 0);
+    const expensesSum = [...transactions]
+      .filter(t => t.type === 'Expense')
+      .reduce((acc, t) => {
+        return acc + t.sum * 100;
+      }, 0);
+    const newBalance = (incomesSum - expensesSum) / 100;
     setBalance(prev => (prev = newBalance));
-  };
-
-  console.log('typeof balance:', typeof balance);
+  }, [transactions]);
 
   return (
     <div className={css.balanceContainer}>
       <Box className={css.balanceText}>Your Balance</Box>
-      <Box className={css.balanceBox}>₴ {formatMoney(balance)}</Box>
+      <Box className={css.balanceBox}>₴ <span className={css.balanceSum}>{formatMoney(balance)}</span></Box>
     </div>
   );
 };
