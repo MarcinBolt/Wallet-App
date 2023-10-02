79e3047ef41aed215ca98bd0dfe352f7a8f99136
MarcinBolt <marcin.boltruczyk@gmail.com>
2023-10-02 18:57:10 +0200

    update faker data generator

--- server/utils/transactions.faker.js
+++ server/utils/transactions.faker.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import { faker } from '@faker-js/faker';
 import Transaction from '../service/schema/transaction.schema.js';
+import 'dotenv/config';
 
 mongoose.connect(process.env.DATABASE_URI, {
   useNewUrlParser: true,
@@ -20,21 +21,24 @@ const categories = [
   'Entertainment',
 ];
 
-const types = ['Income', 'Expense'];
+const types = ['Expense'];
+// const types = ['Income', 'Expense'];
 
-const owners = []; // here add strings from mongoDB (objectId) as owners
+const owners = ['']; // here add strings from mongoDB (objectId) as owners
 
 const generateTransactions = async () => {
   const transactions = [];
 
-  for (let i = 0; i < 200; i++) {
-    const date = faker.date.between({ from: '2023-01-20', to: '2023-09-25' });
-    const year = 2023;
-    const month = faker.date.month();
+  for (let i = 0; i < 10; i++) {
+    const iterableDate = faker.date.between({ from: '2021-04-10', to: '2023-10-02' });
+
+    const date = iterableDate;
+    const year = iterableDate.getFullYear();
+    const month = iterableDate.toLocaleString('en-us', { month: 'long' });
     const type = faker.helpers.arrayElement(types);
     const category = faker.helpers.arrayElement(categories);
     const comment = faker.lorem.sentence({ min: 1, max: 7 });
-    const sum = faker.number.int({ min: 1, max: 2000 });
+    const sum = faker.number.float({ min: 5, max: 2000 }).toFixed(2);
     const owner = faker.helpers.arrayElement(owners);
 
     transactions.push({
