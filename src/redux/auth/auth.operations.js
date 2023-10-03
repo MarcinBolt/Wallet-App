257cf71c243774e0a094e252aa1db87332680b2b
MarcinBolt <marcin.boltruczyk@gmail.com>
2023-10-03 12:25:57 +0200

    fix patchs backendBaseURL in auth and transactions operations

--- src/redux/auth/auth.operations.js
+++ src/redux/auth/auth.operations.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import notification from '../../utils/notification.js';
 
-const backendBaseUrl = `${import.meta.env.VITE_BACKEND_SERVER_URL}/#`;
+const backendBaseUrl = `${import.meta.env.VITE_BACKEND_SERVER_URL}`;
 // const backendBaseUrl = (import.meta.env.VITE_NODE_ENV = 'development'
 //   ? `http://127.0.0.1:3000`
 //   : `${import.meta.env.VITE_BACKEND_SERVER_URL}/#`);
