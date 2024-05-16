f40a8aa7e3f38d5199cc3c5fdb25346159acc1d8
MarcinBolt <marcin.boltruczyk@gmail.com>
2024-05-16 19:52:16 +0200

    update base link and port in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Wallet-App/',
+  base: '/',
   plugins: [react()],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.svg'],
+  },
+  server: {
+    port: process.env.PORT || 3000,
   },
 });
