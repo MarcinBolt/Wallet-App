1d3b4ac81ded2a310b657c5d2e69dcd4b1c6b8ba
MarcinBolt <marcin.boltruczyk@gmail.com>
2024-05-16 19:24:54 +0200

    add extension to vite configuration file

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   base: '/Wallet-App/',
   plugins: [react()],
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.svg'],
+  },
 });
