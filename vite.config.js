ec12793591b308d466b7f7bf63698a22d3f727dc
MarcinBolt <marcin.boltruczyk@gmail.com>
2024-05-17 22:51:30 +0200

    add condition in vite config - base depending on current branch

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { execSync } from 'child_process';
+
+// Get the current branch
+const currentBranch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: currentBranch === 'gh-pages' ? '/Wallet-App/' : '/',
   plugins: [react()],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.svg'],
   },
   server: {
     port: (process.env.NODE_ENV === 'production' ? process.env.PORT : 5173) || 4000,
-    // strictPort: true,
-    // proxy: {
-    //   '/api': {
-    //     target: process.env.BACKEND_SERVER_URL,
-    //     changeOrigin: true,
-    //     rewrite: path => path.replace(/^\/api/, ''),
-    //   },
-    // },
   },
 });
